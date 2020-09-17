06d1178d4f6ae98d2c525c73bb3b34371eea9f39
Rajnish Singh <rajnishsingh.n@gmail.com>
2020-09-18 00:05:06 +0530

    as

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 require("dotenv").config();
 const Telegraf = require("telegraf");
 const axios = require("axios");
+
 const opt = ['art', 'attitude', 'family', 'travel', 'beauty', 'books', 'birthday', 'startup', 'success'];
 
 
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
+
 async function find(cat) {
+    try {
+        const url = `https://captionss.herokuapp.com/api/${cat}`;
+        console.log(url);
+        const response = await axios.get(url);
+        const data = await response.data;
 
-
-    const response = await axios.get(`https://captionss.herokuapp.com/api/${cat}/`);
-    const data = await response.data;
-    console.log(cat);
-    return data;
+        return data;
+    } catch (err) {
+        console.error(err);
+    }
 
 }
+
 bot.command('/start', ctx =>
-    ctx.reply(`Welcome To CaptionPlus Unofficial Bot Api. \n The Bot is currently in Dev Mode \n List of commands are \n /attitude  \n /art \n /family \n /success \n /startup \n /birthday \n /travel \n /books \n /beauty`)
+    ctx.reply(`Welcome To CaptionPlus Unofficial Bot Api. \n The Bot is currently in Dev Mode \n List of commands are \n /attitude  \n /art \n /family \n /success \n /startup \n /birthday \n /travel \n /books \n /beauty `)
 )
 
 
@@ -27,15 +34,17 @@ bot.on('text', async ctx => {
     console.log(cat);
 
     if (opt.includes(cat)) {
-        find(`${cat} `).then(data => {
-            let no = Math.floor(Math.random() * (data.total - 0)) + 0;
-            if (!data.total == 0) {
-                ctx.reply(data.captions[no].caption)
-            } else {
-                ctx.reply('Oops No Captions Found')
-            }
 
-        });
+        find(`${cat} `)
+            .then(data => {
+                let no = Math.floor(Math.random() * (data.total - 0)) + 0;
+                if (!data.total == 0) {
+                    ctx.reply(data.captions[no].caption)
+                } else {
+                    ctx.reply('Oops No Captions Found')
+                }
+
+            });
 
     } else {
         ctx.reply('Not a  valid cateogry command should start with /')
