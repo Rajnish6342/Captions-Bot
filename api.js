27cc952ff90142f3aed48ab2ad2c829eee8ae8e5
Rajnish Singh <rajnishsingh.n@gmail.com>
2020-09-19 18:07:01 +0530

    git commit

--- api.js
+++
@@ -1,3 +0,0 @@
-const axios = require("axios");
-const instance = axios.create({ baseURL: 'https://captionss.herokuapp.com/' });
-module.exports = instance;
--- endpoints.js
+++
@@ -1,6 +0,0 @@
-module.exports = (req, res) => {
-  var endpoints = new Object();
-  (endpoints["status"] = "Bot is Running Currently "),
-    
-    res.json(endpoints);
-};
